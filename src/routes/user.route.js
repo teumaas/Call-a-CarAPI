e91017bf53f8bdc706ae513b16dc610cd6acdc4d
Tom Smits <tmsmits@gmail.com>
2021-04-07 16:52:09 +0200

    Fix

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -6,6 +6,6 @@ const { IsAdmin, verifyUserToken } = require("../middlewares/auth.middleware");
 routes.post('/users/register', userController.register);
 routes.get("/users", verifyUserToken, IsAdmin,  userController.getAll);
 routes.post('/users/login', userController.login);
-routes.get("/users/profile", verifyUserToken, userController.getUser);
+routes.get("/users/specific", verifyUserToken, userController.getUser);
 
 module.exports = routes;
