8d778d6882419e3a9f558bd11e6d6e0d87527fe0
Tom Smits <tmsmits@gmail.com>
2021-04-07 17:29:17 +0200

    Fixed admin permission

--- src/routes/ride.route.js
+++ src/routes/ride.route.js
@@ -5,7 +5,7 @@ const { IsAdmin, verifyUserToken } = require("../middlewares/auth.middleware");
 
 routes.get("/rides", verifyUserToken, rideController.getRides);
 routes.post("/rides", verifyUserToken, rideController.postRide);
-routes.put("/rides/:id", verifyUserToken, rideController.putRide);
+routes.put("/rides/:id", verifyUserToken, IsAdmin, rideController.putRide);
 routes.put("/rides/:id/pay", verifyUserToken, rideController.payRideById);
 routes.put("/rides/:id/cancel", verifyUserToken, rideController.cancelRide);
 
