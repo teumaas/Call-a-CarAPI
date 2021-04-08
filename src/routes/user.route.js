1b707ee91a3a21d8d034f210e7db624c84201b24
Tom Smits <tmsmits@gmail.com>
2021-04-08 13:48:00 +0200

    Wrong request methode fix
    - User

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -7,8 +7,8 @@ routes.post('/users/register', userController.register);
 routes.get("/users", verifyUserToken, IsAdmin,  userController.getAll);
 routes.post('/users/login', userController.login);
 routes.get("/users/specific", verifyUserToken, userController.getUser);
-routes.post("/users/token", verifyUserToken, userController.updatePayment);
-routes.post("/users/update", verifyUserToken, userController.updateUser);
+routes.put("/users/token", verifyUserToken, userController.updatePayment);
+routes.put("/users/update", verifyUserToken, userController.updateUser);
 
 
 module.exports = routes;
