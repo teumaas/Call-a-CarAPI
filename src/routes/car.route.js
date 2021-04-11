7d924c3f49e6791d26b4ad013817d54f123a1aad
Tom Smits <tmsmits@gmail.com>
2021-04-11 13:54:52 +0200

    Fixed route sort

--- src/routes/car.route.js
+++ src/routes/car.route.js
@@ -3,15 +3,12 @@ const routes = Router();
 const carController = require('../controllers/car.controller');
 const { IsAdmin, verifyUserToken } = require("../middlewares/auth.middleware");
 
+routes.get("/cars/types/:id", verifyUserToken, IsAdmin, carController.getCarTypeByID);
+routes.put("/cars/types/:id", verifyUserToken, IsAdmin, carController.putCarTypeByID);
 routes.get("/cars/types", verifyUserToken, carController.getCarTypes);
-routes.get("/cars", verifyUserToken, carController.getCars);
 
 routes.get("/cars/:id", verifyUserToken, IsAdmin, carController.getCarByID);
 routes.put("/cars/:id", verifyUserToken, IsAdmin, carController.putCarByID);
-
-routes.get("/cars/type/:id", verifyUserToken, IsAdmin, carController.getCarTypeByID);
-routes.put("/cars/type/:id", verifyUserToken, IsAdmin, carController.putCarTypeByID);
-
-
+routes.get("/cars", verifyUserToken, carController.getCars);
 
 module.exports = routes;
--- src/routes/ride.route.js
+++ src/routes/ride.route.js
@@ -3,13 +3,13 @@ const routes = Router();
 const rideController = require('../controllers/ride.controller');
 const { IsAdmin, verifyUserToken } = require("../middlewares/auth.middleware");
 
-routes.get("/rides", verifyUserToken, rideController.getRidesById);
 routes.get("/rides/all", verifyUserToken, IsAdmin, rideController.getRidesById);
-routes.get("/rides/:id/all", verifyUserToken, IsAdmin, rideController.getRidesByUserId);
-routes.post("/rides", verifyUserToken, rideController.postRide);
-routes.put("/rides/:id", verifyUserToken, IsAdmin, rideController.putRide);
 routes.put("/rides/:id/pay", verifyUserToken, rideController.payRideById);
 routes.put("/rides/:id/cancel", verifyUserToken, rideController.cancelRide);
 routes.put("/rides/:id/setLock", verifyUserToken, rideController.setLock);
+routes.get("/rides/:id/all", verifyUserToken, IsAdmin, rideController.getRidesByUserId);
+routes.put("/rides/:id", verifyUserToken, IsAdmin, rideController.putRide);
+routes.get("/rides", verifyUserToken, rideController.getRidesById);
+routes.post("/rides", verifyUserToken, rideController.postRide);
 
 module.exports = routes;
